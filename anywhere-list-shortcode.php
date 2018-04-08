27e3b2db559607835d257cc1b9075d5f56d4c676
Shuhei Nishimura <nishimura@digitalcube.jp>
2018-04-09 02:38:45 +0900

    remove unnecessary files

--- anywhere-list-shortcode.php
+++ anywhere-list-shortcode.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Anywhere Post List Shortcode
-Version: 0.2
+Version: 0.4
 Plugin URI: http://hibou-web.com
 Description: You can simply put a "[list]" short code, to list the title of the latest article (link with) and the front page of your blog, sidebar, footer. Set category and custom post type, taxonomy also other it is also possible.
 Author: Shuhei Nishimura
-Author URI: http://hibou-web.com
+Author URI: https://private.hibou-web.com
 License: GPLv2 or later
 License URI: http://opensource.org/licenses/gpl-2.0.php GPLv2
 */
@@ -14,7 +14,7 @@ License URI: http://opensource.org/licenses/gpl-2.0.php GPLv2
  * Anywhere Post List Shortcode
  *
  * @package Anywhere Post List Shortcode
- * @version 0.2
+ * @version 0.4
  * @author Shuhei Nishimura <shuhei.nishimura@gmail.com>
  * @copyright Copyright (c) 2014 Shuhei Nishimura (Hibou).
  * @license http://opensource.org/licenses/gpl-2.0.php GPLv2
@@ -22,102 +22,126 @@ License URI: http://opensource.org/licenses/gpl-2.0.php GPLv2
  */
 
 function anywhere_list( $atts ) {
+
 	global $post;
 	extract( shortcode_atts( array(
-		'post_type'			=>	'post',
-		'cat_name'			=>	'',
-		'num'						=>	10,
-		'class'					=>	'',
-		'orderby'				=>	'post_date',
-		'order'					=>	'DESC',
-		'length'				=>	'',
-		'end_of_title'	=>	'',
-		'taxonomy'			=>	'',
-		'term'					=>	'',
-		'more'					=>	' … more',
-		'post_format'		=>	'standard',
-		'no_filter'			=>	false,
+		'post_type'    => 'post',
+		'cat_name'     => '',
+		'num'          => 10,
+		'class'        => '',
+		'orderby'      => 'post_date',
+		'order'        => 'DESC',
+		'length'       => '',
+		'taxonomy'     => '',
+		'term'         => '',
+		'field'        => '',
+		'more'         => '…',
+		'post_format'  => 'standard',
+		'thumbnail'    => 'on',
+		'size'         => 'thumbnail',
+		'slider_mode'  => '',
 	), $atts ) );
 
-	if( $post_type ) {
+	if ( ! empty( $taxonomy ) && ! empty( $term ) ) {
+
 		$args = array(
-		  'post_type'				=>	$post_type,
-		  'posts_per_page'	=>	$num,
-		  'order'						=>	$order,
-		  'category_name'		=>	$cat_name,
-		  'orderby'					=>	$orderby,
-		  'order'						=>	$order,
-		);
-	} elseif( ! empty( $taxonomy ) && ! empty( $term ) ) {
-		$args = array(
-		  'post_type'			=>	$post_type,
-		  'posts_per_page'=>	$num,
-		  'orderby'				=>	$orderby,
-		  'order'					=>	$order,
-		  'tax_query' => array(
+			'post_type'      => $post_type,
+			'posts_per_page' => $num,
+			'orderby'        => $orderby,
+			'order'          => $order,
+			'tax_query'      => array(
 				array(
-					'taxonomy'	=>	$taxonomy,
-					'field'			=>	'slug',
-					'terms'			=>	$term,
+					'taxonomy' => $taxonomy,
+					'field'    => $field,
+					'terms'    => $term,
 				)
 			)
 		);
+
 	} else {
+
 		$args = array(
-		  'post_type'			=>	$post_type,
-		  'posts_per_page'=>	$num,
-		  'order'					=>	$order,
-		  'post_format'		=>	$post_format,
+			'post_type'      => $post_type,
+			'cat_name'       => $cat_name,
+			'posts_per_page' => $num,
+			'order'          => $order,
+			'orderby'        => $orderby,
 		);
+
 	}
 	$list_posts = get_posts( $args );
 
 	$html   = '';
-	$output = '';
-	$class = ( ! empty( $class ) ) ? "class='$class'" : '';
-	if( $list_posts ) {
-		$html .= apply_filters( 'anywhter_before_list_tag', "<ul $class>" );
-		foreach( $list_posts as $post ) {
-			setup_postdata( $post );
-			$post_id    = $post->ID;
-			$post_link	= get_permalink( $post_id );
-			$post_title	= get_the_title( $post_id );
+	$class  = ( ! empty( $class ) ) ? "class='$class'" : '';
 
-			if( ! empty( $length ) && mb_strlen( $post_title ) >= $length ) {
-				$post_title = wp_trim_words( $post_title, $length, $more );
-			}
-			$post_title = apply_filters( 'the_title', $post_title );
+	if ( empty( $list_posts ) ) {
+		return;
+	}
 
-			if( ! empty( $list_posts ) ) {
-				$no_filter ?
-				$html .= apply_filters( 'anywhter_list_content', '<li>'
-							. '<a href="' . esc_url ( $post_link ) . '">'
-							. esc_html( $post_title )
-							. '</a></li>'
-				) :
-				$html .= '<li>'
-							. '<a href="' . esc_url ( $post_link ) . '">'
-							. esc_html( $post_title )
-							. '</a></li>';
-			} else {
-				$no_filter ?
-				$html .= apply_filters( 'anywhter_list_content', '<li>'
-							. '<a href="' . esc_url ( $post_link )
-							. '">'
-							. esc_html( $post_title )
-							. '</a></li>'
-				) :
-				$html .= '<li>'
-							. '<a href="' . esc_url ( $post_link )
-							. '">'
-							. esc_html( $post_title )
-							. '</a></li>';
-			}
+	$html .= '<ul ' .  $class . '>' . "\n";
+
+	foreach ( $list_posts as $post ) {
+
+		setup_postdata( $post );
+		$post_id    = $post->ID;
+		$post_link  = get_permalink( $post_id );
+		$post_title = get_the_title( $post_id );
+		if ( (int)$length > 0  ) {
+
+			$post_title = wp_trim_words( wp_strip_all_tags( $post_title ), intval( $length ), $more );
+
+		} else {
+
+			$post_title = get_the_title( $post_id );
+
 		}
+
+		if( $thumbnail === 'on' && has_post_thumbnail( $post->ID ) ) {
+
+			$thumb_attr = array(
+				'title' => trim( wp_strip_all_tags( $post_title ) ),
+			);
+			$post_thumbnail =  get_the_post_thumbnail( $post->ID, $size, $thumb_attr );
+
+		} else {
+
+			$post_thumbnail = false;
+
+		}
+
+		if ( ! empty( $length ) && mb_strlen( $post_title ) >= $length ) {
+
+			$post_title = wp_trim_words( $post_title, $length, $more );
+
+		}
+
+		$html .= '<li>' . "\n";
+		$html .= '<a href="' . esc_url( $post_link ) . '" title="' . wp_strip_all_tags( $post_title ) . '">' . "\n";
+		if ( $slider_mode === 'on' ) {
+
+			$html .= $post_thumbnail;
+
+		} else {
+
+			if ( $post_thumbnail ) {
+				$html .= '<span class="post_thumb">' . "\n";
+				$html .= $post_thumbnail . "\n";
+				$html .= '</span>' . "\n";
+			}
+			$html .= '<span class="post_title">' . "\n";
+			$html .= esc_html( $post_title ) . "\n";
+			$html .= '</span>' . "\n";
+
+		}
+		$html .= '</a>' . "\n";
+		$html .= '</li>' . "\n";
+
 	}
 	wp_reset_postdata();
-	$html .= apply_filters( 'anywhter_after_list_tag', '</ul>' );
+	$html .= '</ul>' . "\n";
+
 	return $html;
+
 }
 add_shortcode( 'list', 'anywhere_list' );
 
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: marushu
 Tags: post, post type
 Requires at least: 3.9.1
-Tested up to: 4.2.2
+Tested up to: 4.9.5
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -15,47 +15,54 @@ List the article :)
 = Fetures =
 - WordPress post retrieve and list.
 - Purpose to be output by the number specified latest posts mainly is suitable.
-- 【NEW 0.2】Added filter( anywhter_before_list_tag, anywhter_list_content, anywhter_after_list_tag ).
 
 You can simply put a "[list]" short code, to list the title of the latest article (link with) and the front page of your blog, sidebar, footer. Set category and custom post type, taxonomy also other it is also possible.
 
 = Parameters =
-Main parameters, [refer to the get_posts of Codex](http://codex.wordpress.org/Template_Tags/get_posts).  
+Main parameters, [refer to the get_posts of Codex](http://codex.wordpress.org/Template_Tags/get_posts).
 
-- post_type: WordPress post type. Default is post.  
-- cat_name: Category slug.  
-- num: Number: The number of posts you want to output.  
-- class: Unorderd list's class name. maybe easy controllable :)  
-- orderby: Post's order.  
-- order: Ascending Descending.  
-- length: If you want to adjust the length of post title  
-- end_of_title: Specify a string to be appended to the end of the article. This must be required for 'length'.  
-- taxonomy: If you want to output by specifying a custom taxonomy.  
-- term: Required for 'taxonomy'.  
-- more: read more link text.
+- post_type: WordPress post type. Default is post.(Default: post)
+- cat_name: Category slug.( Default: '' )
+- num: Number: The number of posts you want to output.  ( Default: 10 )
+- class: Unorderd list's class name. maybe easy controllable :)  ( Default: '' )
+- orderby: Post's order.  ( Default: post_date )
+- order: Ascending Descending.  ( Default: DESC )
+- length: If you want to adjust the length of post title.( Default: empty )
+- taxonomy: If you want to output by specifying a custom taxonomy.  ( Default: empty )
+- term: Required for 'taxonomy'.  ( Default: empty )
+- more: read more link text.( Default: … )
 - post_format: post format
-- no_filter(boolean): If you are creating a filter hook , you are able to set not affected. :)
+- thumbnail: Display post thumbnail.( Default: on )
+- slider_mode: If 'on', display only image.( Default: empty )
 
 = Example =
-- [list] most simple.  
-- [list post_type=post cat_name=news num=5 class=newslist] at visual editor.  
-- `<?php echo do_shortcode( '[list]' ); ?>` at template.  
+- [list] most simple.
+- [list post_type=post cat_name=news num=5 class=newslist] at visual editor.
+- `<?php echo do_shortcode( '[list]' ); ?>` at template.
 
 = Other =
 It can also be used in widget :)
 
 == Installation ==
-- A plug-in installation screen is displayed on the WordPress admin panel.  
-- It installs it in `wp-content/plugins`.  
-- The plug-in is made effective.  
+- A plug-in installation screen is displayed on the WordPress admin panel.
+- It installs it in `wp-content/plugins`.
+- The plug-in is made effective.
 
 == Changelog ==
+
+= 0.4 =
+- bug fix
+
+= 0.3 =
+- add post thumbnail
+- remove unnecessary code(add filter etc)
+
 = 0.2 =
 - add filter
 - no_filter option
 
 = 0.1 =
-- First release :)  
+- First release :)
 
 == Contact ==
 Twitter: @marushu
